6099ec3f0481e3e0faf7f11f6ebc50a439051423
luuk <info@deluuxe.nl>
2018-03-31 16:31:59 +0200

    Add files via upload

---
+++ DiscordDeluuxeEdition.plugin.js
@@ -0,0 +1,61 @@
+//META{"name":"DiscordDeluuxeEdition"}*//
+
+/* global PluginUtilities:false, InternalUtilities:false, BdApi:false */
+
+class DiscordDeluuxeEdition {
+	getName() { return "DiscordDeluuxeEdition"; }
+	getShortName() { return "DDE"; }
+	getDescription() { return "Adds custom discord style by deluuxe. AUTOMAGICALY UPDATES"; }
+	getVersion() { return "1.0"; }
+	getAuthor() { return "DELUUXE#1608"; }
+
+	constructor() {
+		this.initialized = false;
+	}
+	
+	load() {}
+	unload() {}
+	
+	start() {
+		var libraryScript = document.getElementById('LibraryScript');
+		if (libraryScript) libraryScript.parentElement.removeChild(libraryScript);
+		libraryScript = document.createElement("script");
+		libraryScript.setAttribute("type", "text/javascript");
+		libraryScript.setAttribute("src", "https://rauenzi.github.io/BetterDiscordAddons/Plugins/PluginLibrary.js");
+		libraryScript.setAttribute("id", "LibraryScript");
+		document.head.appendChild(libraryScript);
+
+		if (typeof window.Library !== "undefined") this.initialize();
+		else libraryScript.addEventListener("load", () => { this.initialize(); });
+	}
+
+	initialize() {
+        let fs = require('fs');
+		this.initialized = true;
+        var DDEcss = 'DDEcss';
+        if (!document.getElementById(DDEcss))
+        {
+            var head  = document.getElementsByTagName('head')[0];
+            var link  = document.createElement('link');
+            link.id   = DDEcss;
+            link.rel  = 'stylesheet';
+            link.type = 'text/css';
+            link.href = 'https://deluuxe.github.io/DiscordDeluuxeEdition/DiscordDeluuxeEdition.css';
+            link.media = 'all';
+            head.appendChild(link);
+        }
+		PluginUtilities.showToast(this.getName() + " has started.");
+	}
+
+	stop() {
+        //BdApi.clearCSS(this.getShortName());
+        if(document.getElementById(DDEcss)){
+            document.head.removeChild(document.getElementById(DDEcss));
+        }
+    }
+
+	getSettingsPanel() {
+		return null;
+	}
+}
+
